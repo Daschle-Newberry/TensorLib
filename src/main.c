8bd8911779ef07a92a669643e4458f13675f5675
Fiveberries <dash.q.n@gmail.com>
2025-10-12 21:17:26 -0700

    <repo-fix> Fixed main.c not committed

--- src/main.c
+++ src/main.c
@@ -4,51 +4,36 @@
 #include <time.h>
 
 #include "tensor.h"
-#include "tensor_operations.h"
+
+
+
+
 
 int main(void) {
+    Tensor out;
     Tensor a;
     Tensor b;
-    int size = 100;
-    int data_length = size*size;
-    float data_a[data_length];
-    int shape_a[2] = {size,size};
+
+
+    float data_a[] = {2,2,2,2,2,2,2,2,2};
+    int shape_a[2] = {3,3};
     int ndim_a = 2;
 
-    float data_b[data_length];
-    int shape_b[2] = {size,size};
+    float data_b[6] = {1,1,1,1,1,1};
+    int shape_b[2] = {2,3};
     int ndim_b = 2;
 
-    for (int i = 0; i < data_length; i++) {
-        data_a[i] = (float)i;
-        data_b[i] = (float)i;
-    }
 
 
-    TensorError err = tensor_init(&a, data_a, shape_a, ndim_a);
-    printf("A init error: %s\n", tensor_error_to_string(err));
-    printf("A\n %s", tensor_metadata_to_string(&a));
 
-    err = tensor_init(&b, data_b, shape_b, ndim_b);
-    printf("B init error: %s\n", tensor_error_to_string(err));
-    printf("B\n %s", tensor_metadata_to_string(&b));
+    TensorError err = tensor_from_data(&a, data_a, shape_a, ndim_a);
+    // printf("A init error: %s\n", tensor_error_to_string(err));
+    // printf("A\n %s", tensor_metadata_to_string(&a));
 
+    printf("%s\n",tensor_error_to_string(err));
 
-    float count = 5000;
-
-    float i = 0;
+    printf("%s\n", tensor_to_string(&a));
 
 
-    clock_t start = clock();
-    while (i < count) {
-        Tensor res;
-        mul_tensor(&res, &a, &b);
-        i++;
-    }
-
-    clock_t end = clock();
-
-    double total_time = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("%f operations per second", count/total_time);
     return 0;
 }
--- src/tensor.c
+++ src/tensor.c
@@ -3,6 +3,10 @@
 
 #include "tensor.h"
 
+#include <stdio.h>
+
+#include "string_builder.h"
+
 static const char* TensorErrorStrings[] = {
     [TENSOR_ERROR_NONE] = "TENSOR_ERROR_NONE",
     [TENSOR_ERROR_NO_MEMORY] = "TENSOR_ERROR_NO_MEMORY",
@@ -57,6 +61,29 @@ static int tensor_alloc(Tensor* out, const int* shape, const int ndim) {
     out->length = flat_length;
 
     return 0;
+}
+
+static void build_string(StringBuilder* sb, const Tensor* tensor, const int offset, const int dim) {
+    if (dim == tensor->ndim - 1) {
+        sb_append(sb, "     [");
+        for (int i = 0; i <tensor->shape[dim]; i++) {
+            char buff[32];
+            snprintf(buff, sizeof(buff),"%.6g",tensor->data[offset + i]);
+            sb_append(sb,buff);
+
+            if (i < tensor->shape[dim]-1)
+                sb_append(sb,", ");
+        }
+        sb_append(sb,"]\n");
+        return;
+    }
+
+    sb_append(sb,"[\n");
+    for (int i = 0; i < tensor->shape[dim]; i++) {
+        const int new_offset = offset + i * tensor->strides[dim];
+        build_string(sb, tensor, new_offset, dim + 1);
+    }
+    sb_append(sb,"]\n");
 }
 
 TensorError tensor_empty(Tensor* out, const int* shape, const int ndim) {
@@ -134,10 +161,62 @@ TensorError tensor_expand(Tensor* out, const Tensor* in, const int* new_shape, c
 
 }
 
-TensorError tensor_promote_to_col(Tensor* out, const Tensor* in);
+TensorError tensor_promote_to_col(Tensor* out, const Tensor* in) {
+    const int m = tensor->shape[tensor->ndim - 2];
+    const int n = tensor->shape[tensor->ndim -1];
 
-const char* tensor_to_string(const Tensor* tensor);
-const char* tensor_metadata_to_string(const Tensor* tensor);
-const char* tensor_error_to_string(TensorError error);
+    const int ms = tensor->strides[tensor->ndim - 2];
+    const int ns = tensor->strides[tensor->ndim - 1];
+
+    tensor->shape[tensor->ndim - 2] = n;
+    tensor->shape[tensor->ndim - 1] = m;
+    tensor->strides[tensor->ndim - 2] = ns;
+    tensor->strides[tensor->ndim - 1] = ms;
+
+    return TENSOR_ERROR_NONE;
+}
+
+const char* tensor_to_string(const Tensor* tensor) {
+    StringBuilder sb;
+    init_sb(&sb);
+    build_string(&sb,tensor, 0, 0);
+    return sb.buff;
+}
+
+const char* tensor_metadata_to_string(const Tensor* tensor) {
+    StringBuilder sb;
+    init_sb(&sb);
+
+    sb_append(&sb, "----------- METADATA -----------\n");
+    char ndim_buff[21];
+    snprintf(ndim_buff, sizeof(ndim_buff),"%d",tensor->ndim);
+
+    sb_append(&sb, "ndim: ");
+    sb_append(&sb, ndim_buff);
+    sb_append(&sb, "\nShape: [");
+
+    for (int i = 0; i < tensor->ndim; i++) {
+        char shape_buff[21];
+        snprintf(shape_buff, sizeof(shape_buff),"%d",tensor->shape[i]);
+
+        sb_append(&sb, shape_buff);
+        if (i < tensor->ndim - 1) {
+            sb_append(&sb, ", ");
+        }
+    }
+
+    sb_append(&sb, "]\nStrides: [");
+
+    for (int i = 0; i < tensor->ndim; i++) {
+        char stride_buff[21];
+        snprintf(stride_buff, sizeof(stride_buff),"%d",tensor->strides[i]);
+        sb_append(&sb, stride_buff);
+        if (i < tensor->ndim - 1) {
+            sb_append(&sb, ", ");
+        }
+    }
+}
+
+const char* tensor_error_to_string(const TensorError error){return TensorErrorStrings[error];}
 
 
